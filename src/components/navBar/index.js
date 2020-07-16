f50ecec6fc6d4a0f1476ee3e9805f5fe057beffa
Kent Chadwick <kento.mc@gmail.com>
2020-07-16 15:48:34 +0100

    Edited nav bar options

--- src/components/navBar/index.js
+++ src/components/navBar/index.js
@@ -11,21 +11,29 @@ export default class NavBar extends Component {
 
     return (
       <Menu inverted>
-        <Menu.Item
-          name='home'
-          active={activeItem === 'home'}
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          name='messages'
-          active={activeItem === 'messages'}
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          name='friends'
-          active={activeItem === 'friends'}
-          onClick={this.handleItemClick}
-        />
+        <header class="header item">Points of Interest</header>
+        <div class="right menu">
+          <Menu.Item
+            name='dashboard'
+            active={activeItem === 'dashboard'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name="pois"
+            active={activeItem === 'pois'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='settings'
+            active={activeItem === 'settings'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='log out'
+            active={activeItem === 'log out'}
+            onClick={this.handleItemClick}
+          />
+        </div>
       </Menu>
     )
   }
